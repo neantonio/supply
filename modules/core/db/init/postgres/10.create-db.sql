24dbf7b2be9e81729d22bb3ef1d936c853451902
a.sokolov <asokolov@groupstp.ru>
2018-06-15 15:17:57 +0800

    учтена возможность отсутствия флага в makeMovement

--- modules/core/db/init/postgres/10.create-db.sql
+++ modules/core/db/init/postgres/10.create-db.sql
@@ -387,3 +387,21 @@ create table SUPPLY_VOTE (
     primary key (ID)
 )^
 -- end SUPPLY_VOTE
+-- begin SUPPLY_PROCURATION
+create table SUPPLY_PROCURATION (
+    ID uuid,
+    VERSION integer not null,
+    CREATE_TS timestamp,
+    CREATED_BY varchar(50),
+    UPDATE_TS timestamp,
+    UPDATED_BY varchar(50),
+    DELETE_TS timestamp,
+    DELETED_BY varchar(50),
+    --
+    NUMBER_ varchar(20) not null,
+    EMPLOYEE_ID uuid,
+    DATE_ date,
+    --
+    primary key (ID)
+)^
+-- end SUPPLY_PROCURATION
