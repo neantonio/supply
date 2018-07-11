a3665ad3cdd69301aa5587d42864f8d3bbd63310
a.sokolov <asokolov@groupstp.ru>
2018-07-11 10:22:59 +0800

    QueryImportServiceBean.java удалён

---
+++ modules/core/src/com/groupstp/supply/service/QueryImportServiceBean.java
@@ -0,0 +1,8 @@
+package com.groupstp.supply.service;
+
+import org.springframework.stereotype.Service;
+
+@Service(QueryImportService.NAME)
+public class QueryImportServiceBean implements QueryImportService {
+
+}
